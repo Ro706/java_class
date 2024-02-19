18c3b9ed5ebc538166924395985d29e34fd55584
Ro706 <rohitmandal99091@gmail.com>
2024-02-19 12:44:52 +0530

    add

---
+++ My_First_Project/src/A.java
@@ -0,0 +1,10 @@
+public class A {
+	private int num;//Instance variable;
+	public static void main(String[] args) {
+		A a1 = new A();//object  / Instance 
+		//(static content) = (dynamic content
+		a1.num=9;
+		System.out.println("a="+a1.num);
+	}
+}
+      
