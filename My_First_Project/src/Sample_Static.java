092215b625c2426cfff00a1c776ccede294cbcf4
Ro706 <rohitmandal99091@gmail.com>
2024-02-24 17:06:00 +0530

    add

---
+++ My_First_Project/src/Sample_Static.java
@@ -0,0 +1,6 @@
+
+public class Sample_Static {
+	public static void main(String[] args) {
+		System.out.println(Static_Variable_Demo.collegeName);
+	}
+}
---
+++ My_First_Project/src/Static_Variable_Demo.java
@@ -0,0 +1,5 @@
+
+public class Static_Variable_Demo {
+	public static String collegeName ="Symbiosis institute of technology"; 	//Instance variable
+	
+}
