fe160dada6cbe13ccb28a59300d7c7fc0279d9ea
Ro706 <rohitmandal99091@gmail.com>
2024-03-30 15:02:48 +0530

    add array folder

---
+++ Array/src/Example.java
@@ -0,0 +1,22 @@
+public class Example {
+	public static void main(String[] args) {
+		int arr[] = new int[5];
+		double arr2[]= new double[5];
+		String arr3[]= new String[5];
+		System.out.println("Interger type array");
+		for(int i = 0 ; i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+		System.out.println(arr.length);
+		System.out.println("double type array");
+		for(int i = 0 ; i<arr2.length;i++) {
+			System.out.println(arr2[i]);
+		}
+		System.out.println(arr2.length);
+		System.out.println("String type array");
+		for(int i = 0 ; i<arr3.length;i++) {
+			System.out.println(arr3[i]);
+		}
+		System.out.println(arr3.length);
+	}
+}
---
+++ Array/src/Example2.java
@@ -0,0 +1,35 @@
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// interview question
+		int arr[] = {56,98,76,78,12};
+		System.out.println("The Element of array are :");
+		for(int i = 0 ; i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+		System.out.println();
+		int max = arr[0];
+		for (int i = 1 ; i< arr.length;i++) {
+			if (max < arr[i]) {
+				max = arr[i];
+			}
+		}
+		System.out.println(max);
+		int max2 = arr[0];
+		for (int i = 1 ; i< arr.length;i++) {
+			if (max2 < arr[i] && arr[i] != max) {
+				max2 = arr[i];
+			}
+		}
+		System.out.println(max2);
+		int max3 = arr[0];
+		for (int i = 1 ; i< arr.length;i++) {
+			if (max3 < arr[i] && arr[i] != max && arr[i]!= max2) {
+				max3 = arr[i];
+			}
+		}
+		System.out.println(max3);
+	}
+
+}
