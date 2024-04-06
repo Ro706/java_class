88f87ca9da3c8c3df05d029093641ec4f044e0d0
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2024-04-06 14:16:24 +0530

    Create Calc_Factorial.java

---
+++ ExceptionHandlinginJava/src/Calc_Factorial.java
@@ -0,0 +1,14 @@
+public class Calc_Factorial{
+  public static void findFact(int num) throws MyException{
+    if (num < 0){
+      throw new MyException("Can not calculate the factorial of the negative number");
+    }
+    else{
+      int fact =  1;
+      for (int i = 1 ; i <=num;i++){
+        fact = fact*i;
+      }
+      System.out.println("Factorial is = "+fact);
+    }
+  }
+}
