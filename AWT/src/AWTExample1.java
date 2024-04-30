44507c7452ef411a21ffc35bca676c945d33e804
Ro706 <rohitmandal99091@gmail.com>
2024-04-30 15:31:51 +0530

    update

--- AWT/src/AWTExample1.java
+++ AWT/src/AWTExample1.java
@@ -9,11 +9,15 @@ public class AWTExample1 extends Frame {
 		Button b = new Button(a);
 		b.setBounds(100,100,80,30);//(x,y,width,height)
 		add(b);
-		setLayout(null);
-		setVisible(true);
 		Button bb = new Button("Click Clock");
 		bb.setBounds(190,100,80,30);//(x,y,width,height)
 		add(bb);
+		Button bf = new Button("Click food button");
+		bf.setBounds(100,170,120,30);//(x,y,width,height)
+		add(bf);
+		Button bc = new Button("Click bhai");
+		bc.setBounds(220,170,80,30);//(x,y,width,height)
+		add(bc);
 	}
 	public static void main(String[] args) {
 		AWTExample1 f = new AWTExample1("Click me !!");
