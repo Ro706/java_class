ffdffef932034253b049efe3adfcaed4762332a0
Ro706 <rohitmandal99091@gmail.com>
2024-05-02 16:49:22 +0530

    update

--- SwingTech/src/com/JDBC/DatabaseConnectivity.java
+++ SwingTech/src/com/JDBC/DatabaseConnectivity.java
@@ -1,4 +1,9 @@
 package com.JDBC;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import com.mysql.cj.xdevapi.Statement;
 
 public class DatabaseConnectivity {
 
@@ -6,6 +11,11 @@ public class DatabaseConnectivity {
 		try {		
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			System.out.println("Drivers Registered ...");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb","root","root123");
+			System.out.println("Connection Established...");
+			String sql = "insert into tbl_student values(6,'raju','IT')";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.execute();
 		}
 		catch(Exception e) {
 			System.out.print("Exception = "+e);
